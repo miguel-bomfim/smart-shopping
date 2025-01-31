111557b31895acf4db272e232f187d0a1838043f
Miguel Augusto <60234561+miguel-bomfim@users.noreply.github.com>
2025-01-31 01:49:41 +0000

    first commit

--- app/components/ShoppingList.tsx
+++ app/components/ShoppingList.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import { CiCirclePlus, CiTrash, CiCircleCheck } from "react-icons/ci";
+import { FaCirclePlus } from "react-icons/fa6";
 
 interface Item {
   id: number;
@@ -44,23 +45,34 @@ export default function ShoppingList() {
     }
   };
 
+  let itemsPrice = [];
+  itemsPrice.push(items.map((item) => Number(item.price)));
+  console.log("itemsprice", typeof itemsPrice[0]);
+
   if (!showList) {
     return (
       <button
         onClick={() => setShowList(true)}
-        className="w-40 h-40 rounded-full text-lg font-medium"
+        className="flex justify-center items-center border border-gray-500 rounded-lg flex-col w-40 h-40 text-lg font-medium hover:bg-gray-100 transition duration-300 "
       >
-        Nova Lista +
+        Nova Lista <FaCirclePlus size={22} />
       </button>
     );
   }
 
   return (
-    <div className="w-full max-w-md space-y-4">
+    <div className="w-full max-w-md space-y-4 border-black border rounded-lg">
+      <p className="text-center font-bold p-2">
+        Total:{" "}
+        {itemsPrice[0].reduce(
+          (accumulator, currentValue) => accumulator + currentValue,
+          0
+        )}
+      </p>
       {!isAdding ? (
         <button
           onClick={() => setIsAdding(true)}
-          className="w-full py-6 text-lg"
+          className="w-full py-6 text-lg flex items-center justify-center"
         >
           Add <CiCirclePlus className="ml-2" />
         </button>
@@ -94,11 +106,11 @@ export default function ShoppingList() {
         </div>
       )}
 
-      <div className="space-y-2">
+      <div>
         {items.map((item) => (
           <div
             key={item.id}
-            className="flex justify-between items-center p-4 border rounded-lg"
+            className="flex justify-between border border-b-0 border-x-0 border-gray-500 items-center p-4 border "
           >
             {editingId === item.id ? (
               <>
@@ -112,7 +124,7 @@ export default function ShoppingList() {
                       )
                     }
                     className="flex-1 p-2 border rounded"
-                    onKeyPress={(e) =>
+                    onKeyUp={(e) =>
                       e.key === "Enter" &&
                       document.getElementById(`price-edit-${item.id}`)?.focus()
                     }
