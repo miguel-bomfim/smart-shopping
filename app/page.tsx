ef6dba360d61844c3d1768a48065cf7cd76a35fb
Miguel Bomfim <60234561+miguel-bomfim@users.noreply.github.com>
2025-06-07 03:54:12 -0300

    Delete app directory

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import ShoppingList from "@/app/components/ShoppingList";
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center p-8 bg-background">
-      <ShoppingList />
-    </main>
-  );
-}
