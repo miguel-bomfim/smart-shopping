1b7ba8ef2b6f07caf5430edc4111227cab2ba291
miguel-bomfim-gsoul <miguel.bomfim@gsoul.com.br>
2025-07-31 14:17:43 -0300

    fix: create dashboard component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { authGuard } from './guards/auth.guard';
 import { HomeComponent } from './components/home/home.component'
 import { LoginComponent } from './components/login/login.component'
+import { DashboardComponent } from './components/dashboard/dashboard.component'
 
 export const routes: Routes = [
   {
@@ -12,4 +13,5 @@ export const routes: Routes = [
     ]
   },
   { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
 ];
