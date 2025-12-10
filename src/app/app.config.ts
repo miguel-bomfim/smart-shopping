c189f9823950ac2a9ed70a55d6de3d7f567dc850
miguel-bomfim-gsoul <miguel.bomfim@gsoul.com.br>
2025-12-10 19:26:24 -0300

    fix: remove unsused config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,4 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
 import { provideRouter } from '@angular/router';
@@ -10,7 +9,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideAnimationsAsync(),
     providePrimeNG({
       theme: {
         preset: Aura
