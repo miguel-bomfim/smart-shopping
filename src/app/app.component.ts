0df7cd291e7afcdb8466a068afc2e8c0c5d4ea03
miguel-bomfim-gsoul <miguel.bomfim@gsoul.com.br>
2025-12-10 19:26:00 -0300

    fix: update primeng to v21

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterModule } from "@angular/router";
+import { RouterModule, Router, NavigationEnd, RouterOutlet } from "@angular/router";
+import { filter } from 'rxjs';
 import { MenuComponent } from './components/menu/menu.component';
 
 @Component({
 	selector: 'app-root',
-	imports: [RouterModule, MenuComponent],
+	imports: [RouterModule, MenuComponent, RouterOutlet],
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+	showMenu: boolean = true;
+
+  	constructor(private router: Router) {
+		// Monitora as mudanças de rota
+		this.router.events.pipe(
+		filter(event => event instanceof NavigationEnd)
+		).subscribe((event: any) => {
+		// Se a URL for '/login', esconde o menu. Caso contrário, mostra.
+		this.showMenu = event.url !== '/login';
+		});
+	}
+}
